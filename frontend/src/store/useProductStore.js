55b35718faf28ea9b92804bbd0252fa201b89cda
Benjilo12 <benjamindarteyy@gmail.com>
2026-06-18 09:47:56 +0000

    Update environment variables, enhance rate limiting, and configure production settings

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-// base url will be dynamic depending on the environment
+//* base url will be dynamic depending on the environment
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:8000" : "";
 
